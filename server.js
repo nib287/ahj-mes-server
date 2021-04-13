22da9d4b142759976284f9b471217262f408202d
Igor Nikolauk <nib287@yandex.ru>
2021-04-13 20:56:24 +0300

    add: cors

--- server.js
+++ server.js
@@ -14,12 +14,43 @@ app.use(koaBody({
 
 const WS = require('ws');
 const { errorMonitor } = require('events');
-const port = process.env.PORT || 7070;
+const port = process.env.PORT 
 const server = http.createServer(app.callback());
 const wsServer = new WS.Server({server});
 const onlineClients = new Map();
 
 wsServer.on('connection', (ws, req) => {
+    app.use(async(ctx, next) => {
+        const origin = ctx.request.get('Origin');
+        if(!origin) {
+            return await next();
+        }
+        
+        const headers = {'Access-Control-Allow-Origin': '*'}
+        
+        if(ctx.request.method !== 'OPTIONS') {
+            ctx.response.set({...headers});
+            try {
+                return await next();
+            }catch(e) {
+                e.headers = {...e.headers,...headers};
+                throw e;
+            }
+        }
+        
+        if(ctx.request.get('Access-Control-Request-Method')) {
+            ctx.response.set({...headers,'Access-Control-Allow-Methods':'GET, POST, PUT, DELETE, PATCH'
+        });
+            
+        if(ctx.request.get('Access-Control-Request-Headers')) {
+            ctx.response.set('Access-Control-Allow-Headers', ctx.request.get('Access-Control-Allow-Request-Headers'));
+        }
+            
+        ctx.response.status = 204;
+        }
+    });    
+    
+    
     ws.onclose = () =>  {
         const oflineClientName = onlineClients.get(ws);
         const oflineClientObj = {
@@ -136,6 +167,11 @@ wsServer.on('connection', (ws, req) => {
                 break;
         }      
     });
+});
+
+app.use(async(ctx, next) => {
+    ctx.response.set({'Access-Control-Allow-Origin':'*'});
+    return await next();
 });
 
 server.listen(port);
